d62051d7a03d7970ed134b610d2d26e34025b59d
Hong.Quang <nghongquang@gmail.com>
2016-10-28 17:20:50 +0200

    commit on 28.10 17:20

--- index.php
+++ index.php
@@ -1,14 +1,56 @@
 <html>
 <head>
+<title>Shopin A Ecommerce Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Shopin Responsive web template, Bootstrap Web Templates, Flat Web Templates, AndroId Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <!-- Angular JS -->
 
-<!-- MY App -->
 
 <!-- Bootstrap -->
-
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all"/>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script><!-- Tether for Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- MY App -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
 </head>
 <body>
-Hi everybody
+<!--header-->
+<div class="header">
+    <div class="container">
+        <div class="head">
+            <div class="logo">
+                <a href="index.php">
+                    <img src="images/logo.png">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-top">
+        <div class="container">
+            <div class="col-sm-5 header-login offset-xs-2">
+                <ul>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="register.html">Register</a></li>
+                    <li><a href="checkout.html">Checkout</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-5 header-social">
+                <ul>
+                    <li><a href="#"><i></i></a></li>
+                    <li><a href="#"><i class="ic1"></i></a></li>
+                    <li><a href="#"><i class="ic2"></i></a></li>
+                    <li><a href="#"><i class="ic3"></i></a></li>
+                    <li><a href="#"><i class="ic4"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 
 </html>
